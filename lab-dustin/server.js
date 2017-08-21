1e5cd3d8f76fa456d2ee84a6493929c5aa887727
Dustin <dyelland@gmail.com>
2017-08-21 17:21:26 -0500

    duplicate usernames no longer allowed

--- lab-dustin/server.js
+++ lab-dustin/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const net = require('net');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const server = net.createServer();
 const constructor = require('./client-constructor');
 
@@ -11,36 +11,46 @@ const ee = new EE();
 const pool = [];
 
 ee.on('@all',function(sender,message){
+  console.log(`${sender.id} pings @all channel:\r\n> ${message}`);
   pool.forEach(receiver => {
     if (receiver.id === sender.id) return;
 
-    console.log(`Client ${receiver.id} receiving message`);
     receiver.socket.write(`${sender.id}: ${message}`);
   });
 });
 
-ee.on('@nickname',function(sender,name){
-  console.log(`${sender.id} requests username change.`)
+ee.on('@username',function(sender,name){
+  console.log(name,sender.id);
+  var usernameExists = false;
+  pool.forEach(function(user){
+    if (user.id === name){
+      sender.socket.write('Username already exists.');
+      usernameExists = true;
+    }
+  });
+
+  if (!usernameExists){
+    console.log(`${sender.id} changed username to: ${name}`);
+    sender.id = name;
+  }
 });
 
 server.on('connection',function(socket){
   var client = new constructor.Client(socket);
   pool.push(client);
   socket.write(`Welcome. Your ID is ${client.id}.\r\n`);
-  console.log(`New user joined. Users active: ${pool.length}`);
+  console.log(`${client.id} joined. Users active: ${pool.length}`);
 
   socket.on('data',function(data){
     var startCommand = data.toString('utf-8').indexOf('@');
     var endCommand = data.toString('utf-8').indexOf(' ');
     var command = data.toString('utf-8').slice(startCommand,endCommand);
-    console.log(command);
 
     if (command === '@all'){
       ee.emit('@all',client,data.toString().slice(++endCommand));
     }
-    if (command === '@nickname'){
-      console.log('Change nickname');
-      ee.emit('@nickname',client,data.toString().slice(++endCommand));
+    if (command === '@username'){
+      ee.emit('@username',client,data.toString().trim().slice(++endCommand));
     }
   });
 
@@ -51,7 +61,7 @@ server.on('connection',function(socket){
   socket.on('close', function () {
     console.log(`Client ${client.id} has left.`);
     pool.splice(pool.indexOf(client), 1);
-    console.log(`Users active: ${pool.length}`);
+    console.log(`Users active: ${pool}`);
     console.log(pool.map(c => c.id));
   });
 });
