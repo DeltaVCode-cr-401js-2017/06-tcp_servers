bf47127f5f0775f385f3a8a49ad529292eb862e1
nerdbetter <jasontlogan@gmail.com>
2017-08-21 15:08:34 -0500

    chat works for back and forth.

--- jason-lab/server.js
+++ jason-lab/server.js
@@ -2,7 +2,7 @@
 
 const net = require('net');
 const PORT = process.env.port || 3000;
-const server = net.createServer;
+const server = net.createServer();
 
 const EE = require('events');
 const ee = new EE();
@@ -13,32 +13,35 @@ const uuid = require('uuid/v4');
 function Client(socket){
   this.nickname = 'guest';
   this.socket = socket;
-  this.id = uuid.v4();
+  this.id = uuid();
 }
 
-server.listen('PORT', function(){
+server.listen(PORT, function(){
   console.log(`server is listening on ${PORT}`);
+});
+
+ee.on('@all', function (sender, message) {
+  pool.forEach(receiver => {
+    if (receiver.id === sender.id) return;
+
+    console.log(`Client ${receiver.nickname} receiving message`);
+    receiver.socket.write(`${sender.nickname}: ${message}`);
+  });
 });
 
 server.on('connection', function(socket){
   var client = new Client(socket);
   pool.push(client);
   socket.write(`Welcome. Your ID is ${client.id}.\r\n`);
-  socket.write(`Type @nick to change your nickname \r\n
-                Type @close to exit.\r\n
-                Type @dm followed my a User ID or nickname to direct message a user\r\n`);
-  ee.emit('@all', client, `Welcome, ${client.id} to the chat!`);
-});
+  socket.write(`\t Type @nick to change your nickname \t\n Type @close to exit.\t\n Type @dm followed my a User ID or nickname to direct message a user\n`);
 
-ee.on('@all', function (sender, message){
-  pool.forEach(client=>{
-    client.socket.write(`${Client.nickname}: ${message}`);
+  ee.emit('@all', client, `Welcome, ${client.id} to the chat!\n`);
+
+  socket.on('data', function (data) {
+    console.log(data);
+
+    ee.emit('@all', client, data.toString());
+
   });
-});
-socket.on('close', function(socket){
   
 });
-
-socket.on('data', function (data){
-  console.log(data);
-})
