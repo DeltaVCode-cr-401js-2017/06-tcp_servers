4a6c3cfbd38fd40d3ddd192afe6188abdace159a
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-21 13:23:27 -0500

    start server

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const net = require('net');
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+
+server.listen(PORT, function() {
+  console.log(`Listening on ${PORT}`);
+});
