c659f3327098b0bfc386acdbc284abaecbcb3ae7
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-21 16:15:34 -0500

    @dm command and prevent duplicate nicknames being chosen

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -21,13 +21,30 @@ ee.on('@all', function (sender, message) {
 
     console.log(`Client ${receiver.id} receiving message`);
     receiver.socket.write(`${sender.nickname}: ${message}\r\n> `);
+    sender.socket.write(`\r\n> `);
+  });
+});
+
+ee.on('@dm', function (sender, target, message) {
+  clientPool.forEach(receiver => {
+    if(receiver.nickname === target){
+      console.log(`Client ${receiver.id} receiving message`);
+      receiver.socket.write(`dm from ${sender.nickname}: ${message}\r\n> `);
+      sender.socket.write(`\r\n> `);
+    }
   });
 });
 
 ee.on('@nickname', function (sender, newNickname) {
-  sender.nickname = newNickname;
-  console.log(`Client ${sender.id} nickname changed to ${sender.nickname}`);
-  sender.socket.write(`Your nickname has been changed to: ${sender.nickname}\r\n> `);
+  let usedNicknames = clientPool.map(client => client.nickname);
+  if(usedNicknames.indexOf(newNickname) === -1){
+    sender.nickname = newNickname;
+    console.log(`Client ${sender.id} nickname changed to ${sender.nickname}`);
+    sender.socket.write(`Your nickname has been changed to: ${sender.nickname}\r\n> `);
+  }
+  else{
+    sender.socket.write(`${newNickname} is already in use, please try again.\r\n> `);
+  }
 });
 
 server.listen(PORT, function() {
@@ -37,7 +54,7 @@ server.listen(PORT, function() {
 server.on('connection', (socket) => {
   let client = new Client(socket);
   clientPool.push(client);
-  socket.write(`Welcome to chat, your nickname is ${client.nickname}.  Please use '@nickname <newNickname>' to change your nickname.  Please use '@all <message>' to message all users, '@dm <nickname> <message>'  to message a specific user.\r\n> `);
+  socket.write(`Welcome to chat, your nickname is ${client.nickname}.  Please use '@nickname <newNickname>' to change your nickname.  Please use '@all <message>' to message all users, '@dm <nickname> <message>'  to message only a specific user.\r\n> `);
   socket.on('data', function (data) {
     console.log(data);
     dataHandler(data,client);
@@ -53,15 +70,14 @@ server.on('connection', (socket) => {
 function dataHandler(data,client){
   let dataArray = data.toString().split(' ');
   if(dataArray[0] === '@all'){
-    console.log(client);
     console.log(data.toString().replace('@all ', ''));
     ee.emit('@all', client, data.toString().replace('@all ', ''));
   }
   else if(dataArray[0] === '@dm') {
-    ee.emit('@dm', client, dataArray[1], data.toString().replace(`@dm ${dataArray[1]} `, ''));
+    ee.emit('@dm', client, dataArray[1].replace(/\r?\n|\r/gm,''), data.toString().replace(`@dm ${dataArray[1]} `, ''));
   }
   else if(dataArray[0] === '@nickname') {
-    ee.emit('@nickname', client, dataArray[1]);
+    ee.emit('@nickname', client, dataArray[1].replace(/\r?\n|\r/gm,''));
   }
   else {
     client.socket.write(`Sorry, that is not a valid command, try '@all <message>' to message all users, '@dm <nickname> <message>'  to message a specific user, or '@nickname <newNickname>' to change your nickname.\r\n> `);
