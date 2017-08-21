01b4f46088926cc87c9b0fda91d40e45ffc4be4d
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-21 13:52:25 -0500

    require uuid version 4, and make clientPool to hold our clients

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const uuidv4 = require('uuid/v4');
 const net = require('net');
 const PORT = process.env.PORT || 3000;
 const server = net.createServer();
+
+const clientPool = [];
 
 server.listen(PORT, function() {
   console.log(`Listening on ${PORT}`);
