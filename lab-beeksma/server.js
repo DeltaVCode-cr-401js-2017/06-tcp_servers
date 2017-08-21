78ea7261776b0627f1a46eb099bb4199cafab71e
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-21 14:45:51 -0500

    eventemitter emits created

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -4,13 +4,15 @@ const uuidv4 = require('uuid/v4');
 const net = require('net');
 const PORT = process.env.PORT || 3000;
 const server = net.createServer();
+const EE = require('events');
+const ee = new EE();
 
 const clientPool = [];
 
 function Client(socket){
   this.nickname = 'anon';
   this.id = uuidv4;
-  socket;
+  this.socket = socket;
 }
 
 server.listen(PORT, function() {
@@ -22,17 +24,33 @@ server.on('connection', (socket) => {
   clientPool.push(client);
   socket.on('data', function (data) {
     console.log(data);
-    //handle data
+    dataHandler(data,client);
   });
   socket.on('close', function () {
     closeSocket(client);
   });
   socket.on('error', function (err) {
     console.warn();(err);
-    //handle data
   });
 });
 
+function dataHandler(data,client){
+  let dataArray = data.toString().split(' ');
+  if(data.toString()[0] === '@'){
+    if(dataArray[0] === '@all'){
+      ee.emit('@all', client, 'message');
+    }
+    else if(dataArray[0] === '@dm') {
+      ee.emit('@dm', client, 'recipient', 'message');
+    }
+    else if(dataArray[0] === '@nickname') {
+      ee.emit('@nickname', client, 'newNickname');
+    }
+    else {
+      client.socket.write(`Sorry, that is not a valid command, try '@all <message>' to message all users, '@dm <nickname> <message>'  to message a specific user or '@nickname <newNickname>' to change your nickname`);
+    }
+  }
+}
 
 function closeSocket(client){
   if(clientPool.indexOf(client) != -1){
